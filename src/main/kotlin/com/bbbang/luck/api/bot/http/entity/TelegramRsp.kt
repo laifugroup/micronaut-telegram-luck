92ae9de1a51ce48376a31c40c639722b50445df4
laifugroup <laifuygj@gmail.com>
2024-12-13 01:14:47 +0800

    ivite_link

--- src/main/kotlin/com/bbbang/luck/api/bot/http/entity/TelegramRsp.kt
+++ src/main/kotlin/com/bbbang/luck/api/bot/http/entity/TelegramRsp.kt
@@ -18,13 +18,3 @@ data class GetWebhookInfo(val url:String, @JsonProperty("ip_address")val  ipAddr
                     ,@JsonProperty("max_connections")val maxConnections:Int?
                     ,@JsonProperty("allowed_updates") val allowedUpdates:List<String>?) {
 }
-
-
-@Schema()
-@Introspected
-data class ChatInviteLink(val url:String, @JsonProperty("chat_id")val  chatId:Long?
-                          ,@JsonProperty("name")val name:String?
-                          ,@JsonProperty("expire_date")val expireDate:Int?
-                          ,@JsonProperty("member_limit") val memberLimit:Int
-                          ,@JsonProperty("creates_join_request")val createsJoinRequest:Boolean?) {
-}
